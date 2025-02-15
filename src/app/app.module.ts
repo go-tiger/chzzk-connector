3122a4665221999276fc05890af653e3568022f4
go-tiger <dev.gotiger@gmail.com>
2025-02-15 21:07:19 +0900

    feat: 각 리소스 파일 임시 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfig } from 'src/configs/typeorm.config';
+import { DevelopersModule } from 'src/domains/developers/developers.module';
+import { EventsModule } from 'src/domains/events/events.module';
+import { SessionsModule } from 'src/domains/sessions/sessions.module';
+import { TokensModule } from 'src/domains/tokens/tokens.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({ imports: [ConfigModule], inject: [ConfigService], useClass: TypeOrmConfig }),
+    DevelopersModule,
+    EventsModule,
+    SessionsModule,
+    TokensModule,
   ],
   controllers: [AppController],
   providers: [AppService],
