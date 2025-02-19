df033533b6c9d2544175cf49141e118306108311
go-tiger <dev.gotiger@gmail.com>
2025-02-20 04:57:13 +0900

    feat: 스웨거에 auth 버튼 추가

--- src/configs/swagger.config.ts
+++ src/configs/swagger.config.ts
@@ -2,7 +2,11 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule, SwaggerCustomOptions } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
-  const swaggerConfig = new DocumentBuilder().setTitle('Chzzk Connector Swagger').setDescription('API Docs').build();
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Chzzk Connector Swagger')
+    .setDescription('API Docs')
+    .addBearerAuth()
+    .build();
   const swaggerOptions: SwaggerCustomOptions = { swaggerOptions: { persistAuthorization: true } };
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, swaggerDocument, swaggerOptions);
