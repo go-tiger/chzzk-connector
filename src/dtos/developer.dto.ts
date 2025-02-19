3c92abfdb1480963dcc9f121b3eb0f4c7d707242
go-tiger <dev.gotiger@gmail.com>
2025-02-20 05:00:23 +0900

    add: 개발자 응답값에 비밀번호 제외

--- src/dtos/developer.dto.ts
+++ src/dtos/developer.dto.ts
@@ -1,19 +1,24 @@
 import { ApiProperty, OmitType, PartialType, PickType } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class DeveloperDto {
   @ApiProperty({ example: 1 })
+  @Expose()
   id: number;
 
   @ApiProperty({ description: '이메일' })
+  @Expose()
   email: string;
 
   @ApiProperty({ description: '비밀번호' })
   password: string;
 
   @ApiProperty({ example: '12a7764f-0582-456f-a358-884fa70e5caa' })
+  @Expose()
   clientId: string;
 
   @ApiProperty({ example: 'NwelrUQjkYmOuobj8OLrLGlWL5ElxrldhDeXB-PuGFs' })
+  @Expose()
   clientSecret: string;
 }
 
@@ -22,3 +27,5 @@ export class CreateDeveloperDto extends OmitType(DeveloperDto, ['id'] as const) 
 export class UpdateDeveloperDto extends PartialType(CreateDeveloperDto) {}
 
 export class LoginDeveloperDto extends PickType(DeveloperDto, ['email', 'password'] as const) {}
+
+export class DeveloperResDto extends OmitType(DeveloperDto, ['password'] as const) {}
