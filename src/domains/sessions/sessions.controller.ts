db9c1562f9358e1ae052db4dde646e02064b8a23
go-tiger <dev.gotiger@gmail.com>
2025-02-19 21:29:43 +0900

    feat: 세션 목록 조회(클라이언트)

--- src/domains/sessions/sessions.controller.ts
+++ src/domains/sessions/sessions.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { SessionsService } from './sessions.service';
+import { FindSessionDto } from 'src/dtos';
 
 @Controller('sessions')
 export class SessionsController {
@@ -13,5 +14,10 @@ export class SessionsController {
   @Post(':id/user')
   createSessionUser(@Param('id') id: string) {
     return this.sessionsService.createSessionUser(+id);
+  }
+
+  @Get(':id/client')
+  findSessionClient(@Param('id') id: string, @Query() dto: FindSessionDto) {
+    return this.sessionsService.findSessionClient(+id, dto);
   }
 }
--- src/domains/sessions/sessions.service.ts
+++ src/domains/sessions/sessions.service.ts
@@ -8,6 +8,7 @@ import { DevelopersService } from '../developers/developers.service';
 import { firstValueFrom } from 'rxjs';
 import { SessionType } from 'src/enums/session-type.enum';
 import { TokensService } from '../tokens/tokens.service';
+import { FindSessionDto } from 'src/dtos';
 
 @Injectable()
 export class SessionsService {
@@ -63,6 +64,28 @@ export class SessionsService {
       });
       await this.SessionRepository.save(session);
       console.log('🚀 data:', data);
+    } catch (e) {
+      console.log('🚀 e:', e);
+    }
+  }
+
+  async findSessionClient(id: number, dto: FindSessionDto) {
+    const developer = await this.developersService.findOne(id);
+
+    const requestHeader = {
+      'Client-Id': developer.clientId,
+      'Client-Secret': developer.clientSecret,
+      'Content-Type': 'application/json',
+    };
+
+    try {
+      const { data } = await firstValueFrom(
+        this.httpService.get(`${this.chzzkOpenApi}/open/v1/sessions/client`, {
+          headers: requestHeader,
+          params: { size: dto.size, page: dto.page },
+        }),
+      );
+      return data;
     } catch (e) {
       console.log('🚀 e:', e);
     }
