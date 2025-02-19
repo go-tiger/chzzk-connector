7e55bb1709ea2d7cba61da54717f95f3727b939e
go-tiger <dev.gotiger@gmail.com>
2025-02-19 21:44:55 +0900

    fix: 세션 조회 DTO size 기본값

--- src/dtos/find-session.dto.ts
+++ src/dtos/find-session.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class FindSessionDto {
-  @ApiProperty({ example: 1, default: 20, description: '조회할 세션 개수. 최소 1 ~ 최대 50 요청 가능' })
+  @ApiProperty({ example: 20, default: 20, description: '조회할 세션 개수. 최소 1 ~ 최대 50 요청 가능' })
   size: number;
 
   @ApiProperty({ example: 0, default: 0, description: '조회할 페이지. 0부터 조회 가능' })
